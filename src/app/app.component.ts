a1c93ad6656f0e0cd4e63b8748639b580f685567
darshil2002 <97335353+darshil2002@users.noreply.github.com>
2023-04-03 16:26:41 +0530

    chart -simple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { registerables} from 'node_modules/chart.js'
+import Chart from 'chart.js/auto';
+
+Chart.register(...registerables)
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'grp';
+  ngOnInit(): void {
+    this.renderChart();
+  }
+
+  renderChart(){
+    new Chart('myChart', {
+      type: 'pie',
+      data: {
+        labels: ['adidas', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
 }
